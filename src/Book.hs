cb2179a32cf4779c3f798eb2ed2314d7c1d6a6dc
Qendresa Hajdari <qh43141@ubt-uni.net>
2025-04-11 10:54:39 +0200

    Added map function to applyDiscount

--- src/Book.hs
+++ src/Book.hs
@@ -16,3 +16,10 @@ countBooksByGenre (x:xs) genre =
     in if genre `elem` genres x
        then 1 + restCount
        else restCount
+
+
+applyDiscount :: Book -> Book
+applyDiscount book = book { rating = rating book * 0.9 }
+
+booksWithDiscount :: [Book] -> [Book]
+booksWithDiscount books = map applyDiscount books
--- src/Main.hs
+++ src/Main.hs
@@ -9,13 +9,23 @@ main = do
   let book1 = Book { bookId = 1, title = "Book A", author = "Author X", genres = ["Fantasy", "Adventure"], rating = 4.7}
       book2 = Book { bookId = 2, title = "Book B", author = "Author Y", genres = ["Drama"], rating = 4.8}
       book3 = Book { bookId = 3, title = "Book C", author = "Author X", genres = ["Fantasy", "Drama"], rating = 4.8}
+      
+      books = [book1, book2, book3]
+      titles = map title books
+      
+      totalDrama = countBooksByGenre books "Drama"
 
-      totalDrama = countBooksByGenre [book1, book2, book3] "Drama"
+      discountedBooks = map applyDiscount books
+
 
       
       user = User { userId = 1, history = [book1] }
-      similarityMatrix = generateSimilarityMatrix [book1, book2, book3]
+      similarityMatrix = generateSimilarityMatrix books
       recommendations = recommendBooks user similarityMatrix
   
+  putStrLn ("Book titles: " ++ show titles)
   putStrLn ("Recommendations based on user history:" ++ show recommendations)
   putStrLn ("Total number of Drama books: " ++ show totalDrama)
+  putStrLn ("Books with discount: " ++ show discountedBooks)
+
+
